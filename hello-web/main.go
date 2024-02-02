0e22cd58baa1182b8927b6295fdf5238b1c252ba
Tino Goehlert <t.goehlert@gmail.com>
2024-02-02 23:31:20 +0100

    add flake

--- hello-web/main.go
+++ hello-web/main.go
@@ -10,20 +10,41 @@ import (
 	"syscall"
 )
 
+var shouldFailOnHealthz = false
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("request %s from %s\n", r.RequestURI, r.Host)
 	w.Write([]byte("ola world"))
 }
 
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+}
+
 func healthz(w http.ResponseWriter, r *http.Request) {
+	if shouldFailOnHealthz {
+		w.WriteHeader(500)
+		return
+	}
 	w.WriteHeader(200)
+}
+
+func init() {
+	_, shouldFailOnHealthz = os.LookupEnv("FLAKY")
+	if shouldFailOnHealthz {
+		log.Println("whops, i might be flaky O.o")
+	}
 }
 
 func main() {
 	server := &http.Server{Addr: ":8080"}
-
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", notfound)
+	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/healthz", healthz)
+	http.HandleFunc("/flaky", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("i just got flaky :-/"))
+		shouldFailOnHealthz = true
+	})
 
 	stopChan := make(chan os.Signal, 1)
 	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
